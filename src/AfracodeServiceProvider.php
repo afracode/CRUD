35c858882f51c8d74374b9a7ff4e970e85e825d0
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-08-29 22:09:36 +0430

    override ResourceRegistrar

--- src/AfracodeServiceProvider.php
+++ src/AfracodeServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Afracode\CRUD;
 
+use Afracode\CRUD\app\Overrides\CrudRegistrar;
+use Afracode\CRUD\app\Overrides\ResourceRegistrar;
 use Illuminate\Support\ServiceProvider;
 
 class AfracodeServiceProvider extends ServiceProvider
@@ -16,14 +18,16 @@ class AfracodeServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
+
+    public function boot(\Illuminate\Routing\Router $router)
     {
-        $this->mergeConfigFrom(__DIR__ . '/config/crud.php', 'crud');
+        $this->mergeConfigFrom(__DIR__ . '/config/crud.php', 'router');
         include __DIR__ . '/routes/crud.php';
+
+        $registrar = new ResourceRegistrar($this->app['router']);
+
+        $this->app->bind('Illuminate\Routing\ResourceRegistrar', function () use ($registrar) {
+            return $registrar;
+        });
     }
 }
