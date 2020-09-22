6171ee22b35157a6c99e5798879e486f95a2f2a8
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-09-22 19:02:03 +0330

    Added numberFormatPrecision

--- src/helpers.php
+++ src/helpers.php
@@ -60,3 +60,16 @@ if (!function_exists('crudShowLabel')) {
         return $value;
     }
 }
+
+if (!function_exists('numberFormatPrecision')) {
+    function numberFormatPrecision($number, $precision = 2, $separator = '.')
+    {
+        $numberParts = explode($separator, $number);
+        $response = $numberParts[0];
+        if (count($numberParts)>1 && $precision > 0) {
+            $response .= $separator;
+            $response .= substr($numberParts[1], 0, $precision);
+        }
+        return $response;
+    }
+}
