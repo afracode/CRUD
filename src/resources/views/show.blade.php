ba0ee0cc2b5a6aaedb14a1ccd4b521af8aa9292c
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2021-05-13 03:23:51 +0430

    Added show fields

--- src/resources/views/show.blade.php
+++ src/resources/views/show.blade.php
@@ -9,14 +9,41 @@
                     <table class="table">
                         <tbody>
 
-                        @foreach ($crud->getFields() as $field)
+                        @foreach ($crud->fields as $field)
+
+                            @php $f = $crud->getFieldValue($field['name'])  @endphp
+
+
                             <tr>
                                 <td>
                                     <p>{!! crudShowLabel($field) !!}</p>
                                 </td>
-                                <td><span>{{$field['value']}}</span></td>
+
+                                @if(isset($f['type']) && $f['type'] == 'image')
+                                    <td>
+                                        <img src="{{$f['value']}}" alt="{{$f['label']}}" width="200" height="200">
+                                    </td>
+                                @else
+                                    <td>
+                                        @include('crud::show_fields/'.$field['type'])
+                                    </td>
+                                @endif
                             </tr>
+
+
+
+
+
                         @endforeach
+
+                        <tr>
+                            <td>
+                                <a href="/{{$crud->entities}}" class="btn btn-success">نمایش همه</a>
+                                <a href="/{{$crud->entities}}/{{$crud->row->id}}/edit"
+                                   class="btn btn-primary">ویرایش</a>
+
+                            </td>
+                        </tr>
                         </tbody>
                     </table>
                 </div>
